c83e57fdce8536f3c443471646dcdd880850f515
Dominik Polacek <dominikpolaccek@gmail.com>
2024-02-01 15:39:37 +0100

    created Beer entity

---
+++ src/main/java/com/example/unicornbeerproject/model/Beer.java
@@ -0,0 +1,30 @@
+package com.example.unicornbeerproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+    @Id
+    private Long id;
+    private String uid;
+    private String brand;
+    private String name;
+    private String style;
+    private String hop;
+    private String yeast;
+    private String malts;
+    private String ibu;
+    private String alcohol;
+    private String blg;
+    private Integer rating;
+    private String comment;
+
+
+}
