72c1e6553437a3b901732e515b63eae10a3c06bc
Dominik Polacek <dominikpolaccek@gmail.com>
2024-02-01 15:35:55 +0100

    project created

---
+++ src/main/java/com/example/unicornbeerproject/UnicornBeerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.unicornbeerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicornBeerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnicornBeerProjectApplication.class, args);
+    }
+
+}
